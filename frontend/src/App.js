54ef88116bccdc748753f159bc4cf8fb530e19dc
CodeKaito <jerome.decinco@maxwell.mi.it>
2024-02-27 13:42:24 +0100

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import List from './components/List';
+import axios from 'axios';
+import { baseURL } from './utils/constant';
 
-function App() {
+const App = () => {
+
+  const [input, setInput] = useState(" ");
+  const [tasks, setTasks] = useState([]);
+  const [updateUi, setUpdateUI] = useState(false);
+  const [updateId, setUpdateId] = useState(null);
+
+  useEffect(() => {
+    axios.get(`${baseURL}/get`)
+      .then((response) => { 
+        console.log(response.data);
+        setTasks(response.data);
+      })
+      .catch((error) => { console.log(error) })
+  }, [updateUi]);
+
+  const addTask = () => {
+    axios.post(`${baseURL}/save`, {task: input})
+      .then((response) => { 
+        console.log(response.data) 
+        setInput("");
+        setUpdateUI((prevState) => !prevState);
+      })
+      .catch((error) => { console.log(error) 
+    })
+  };
+
+  const updateMode = (id, text) => {
+    setInput(text);
+    setUpdateId(id);
+  };
+
+  const updateTask = () => {
+    axios.put(`${baseURL}/update/${updateId}`, {task: input})
+      .then((response) => {
+        console.log(response.data);
+        setUpdateUI((prevState) => !prevState);
+        setUpdateId(null);
+        setInput("");
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div className='main-container'>
+      <main>
+        <h1>CRUD Operations</h1>
+
+      <div className="input">
+        <input type="text" value={input} onChange={(e) => setInput(e.target.value)} />
+        <button type="submit" onClick={updateId ? updateTask : addTask}>
+          {updateId ? "Update Task" : "Add Task" }
+        </button>
+      </div>
+
+      <ul>
+        {tasks.map(task => 
+          <List key={task._id} id={task._id} task={task.task} setUpdateUI={setUpdateUI} updateMode={updateMode} />
+        )}
+      </ul>
+    </main>
+  </div>
+    
+  )
 }
 
-export default App;
+export default App
