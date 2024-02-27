65a4e03e52c02eefd369613177c844438c68b147
CodeKaito <jerome.decinco@maxwell.mi.it>
2024-02-27 11:37:42 +0100

    Primo commit con frontend e backend

---
+++ backend/Server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const routes = require('./routes/TaskRoute');
+
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cors());
+
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => console.log('Connected to Mongodb'))
+    .catch((err) => console.log(err));
+
+    app.use("/api", routes);
+
+app.get("/codekaito", (req, res) => {
+    res.send('This is the server response');
+})
+
+app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`);
+});
